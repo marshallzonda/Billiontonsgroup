7dc4d2aa69fbfaad2deed0193ea3e818693768e2
unknown <bond.zonda777@gmail.com>
2020-07-01 17:44:23 +0300

    Адаптация меню

--- js/main.js
+++ js/main.js
@@ -37,4 +37,12 @@ $(document).ready(function(){
 		// Убирает отступы елемента адаптивен
 		variableWidth:false,
 	});
+		$('.header__burger').click(function(event){
+			$('.header__burger,.header__menu').toggleClass('active');
+			$('body').toggleClass('lock');
+		});
+		$('.header__list').click(function(event) {
+		   $('.header__burger,.header__menu').removeClass('active');
+		   $('body').removeClass('lock');
+		});
 });
